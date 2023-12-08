ba84b73fc71b2554ba13b2121af7ec2f33b89a53
Santiago Yepes <santiagoyepescarrera@gmail.com>
2023-12-08 12:00:00 +0000

    Draw a pixel on: 2023-12-08T12:00:00.3988Z

--- drawing_in_github.exs
+++ drawing_in_github.exs
@@ -184,15 +184,5 @@ drawing = [
   Enum.map(1..52, fn x -> :rand.uniform(8) end)
 ]
 
-# drawing = [
-#   [5, 0],
-#   [0, 2],
-#   [0, 0],
-#   [0, 0],
-#   [0, 0],
-#   [0, 0],
-#   [0, 0],
-# ]
-
 # GitHubPixelArt.draw_in_github(drawing, ~D[2024-08-18], ~D[2024-08-24])
 GitHubPixelArt.draw_in_github(drawing, ~D[2023-01-01], ~D[2023-12-30])
